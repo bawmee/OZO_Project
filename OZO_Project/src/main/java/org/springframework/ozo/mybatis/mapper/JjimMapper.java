7f7ac52d1252a8baca6da0de33de307dc6e2f757
bawmee <ppr8769@likelion.org>
2019-10-14 15:30:07 +0900

    first

---
+++ OZO_Project/src/main/java/org/springframework/ozo/mybatis/mapper/JjimMapper.java
@@ -0,0 +1,32 @@
+package org.springframework.ozo.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+public interface JjimMapper {
+	
+	@Select("SELECT jjim_check " + 
+			"FROM jjim " + 
+			"WHERE memid = #{param1} AND space_id = #{param2}")
+	Integer checkJjim(String memId, int space_id);
+	
+	
+	@Select("SELECT COUNT(*) " + 
+			"FROM jjim " + 
+			"WHERE memid = #{param1} AND space_id = #{param2}")
+	Integer isJjim(String memId, int space_id);
+	
+	
+	@Update("UPDATE JJIM SET " +
+			"jjim_check = #{param3} " + 
+			"WHERE memid = #{param1} AND space_id = #{param2}")
+	Integer updateJjim(String memId, int space_id, int jjim_check);
+	
+	@Insert("INSERT INTO JJIM (memid, space_id, jjim_check) " +
+			"VALUES (#{param1}, #{param2}, #{param3}) ")
+	Integer createJjim(String memId, int space_id, int jjim_check);
+	
+	
+	
+}
---
+++ OZO_Project/src/main/java/org/springframework/ozo/service/OzoFacade.java
@@ -0,0 +1,32 @@
+package org.springframework.ozo.service;
+
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.ozo.domain.Credit;
+import org.springframework.ozo.domain.Member;
+
+public interface OzoFacade {
+	
+   Member getMemberBymemId(String memId);
+   
+   List<Credit> getCreditHistory(String memId); 
+   
+   Member getMember(String memId, String password);
+   
+   void insertMember(Member member);
+
+   void updateMember(Member member);
+   
+   void updateProfileImg(String memId, String file);
+   
+   Member getMemberIdByEmail(String memName, String memEmail, String memPhone);
+   
+   public void insertCreditHistory(Credit credit);
+   
+   public Integer checkJjim(String memId, int space_id);
+   
+   public Integer isJjim(String memId, int space_id);
+
+}
